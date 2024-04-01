8b2fa6f3311f91406719709a1a32eee3bad3ab9b
Mihir Suvarna <mihirsuvarna08@gmail.com>
2024-04-01 13:12:53 -0500

    Starting dpager, and cleaned up apager (messy prints)

--- dpager.c
+++ dpager.c
@@ -120,37 +120,37 @@ void print_stack_image(int argc, char **argv) {
  * argv: Expected argument strings
  */
 void stack_check(void* top_of_stack, uint64_t argc, char** argv) {
-    printf("----- stack check -----\n");
+	printf("----- stack check -----\n");
 
-    assert(((uint64_t)top_of_stack) % 8 == 0);
-    printf("top of stack is 8-byte aligned\n");
+	assert(((uint64_t)top_of_stack) % 8 == 0);
+	printf("top of stack is 8-byte aligned\n");
 
-    uint64_t* stack = top_of_stack;
-    uint64_t actual_argc = *(stack++);
-    printf("argc: %lu\n", actual_argc);
-    assert(actual_argc == argc);
+	uint64_t* stack = top_of_stack;
+	uint64_t actual_argc = *(stack++);
+	printf("argc: %lu\n", actual_argc);
+	assert(actual_argc == argc);
 
-    for (int i = 0; i < argc; i++) {
-        char* argp = (char*)*(stack++);
-        assert(strcmp(argp, argv[i]) == 0);
-        printf("arg %d: %s\n", i, argp);
-    }
-    // Argument list ends with null pointer
-    assert(*(stack++) == 0);
+	for (int i = 0; i < argc; i++) {
+		char* argp = (char*)*(stack++);
+		assert(strcmp(argp, argv[i]) == 0);
+		printf("arg %d: %s\n", i, argp);
+	}
+	// Argument list ends with null pointer
+	assert(*(stack++) == 0);
 
-    int envp_count = 0;
-    while (*(stack++) != 0)
-        envp_count++;
+	int envp_count = 0;
+	while (*(stack++) != 0)
+		envp_count++;
 
-    printf("env count: %d\n", envp_count);
+	printf("env count: %d\n", envp_count);
 
-    Elf64_auxv_t* auxv_start = (Elf64_auxv_t*)stack;
-    Elf64_auxv_t* auxv_null = auxv_start;
-    while (auxv_null->a_type != AT_NULL) {
-        auxv_null++;
-    }
-    printf("aux count: %lu\n", auxv_null - auxv_start);
-    printf("----- end stack check -----\n");
+	Elf64_auxv_t* auxv_start = (Elf64_auxv_t*)stack;
+	Elf64_auxv_t* auxv_null = auxv_start;
+	while (auxv_null->a_type != AT_NULL) {
+		auxv_null++;
+	}
+	printf("aux count: %lu\n", auxv_null - auxv_start);
+	printf("----- end stack check -----\n");
 }
 
 void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
