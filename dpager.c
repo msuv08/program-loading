a9df5540c22270db67871f6b5de77e152543a6aa
Mihir Suvarna <mihirsuvarna08@gmail.com>
2024-04-01 22:00:12 -0500

    Attempting to fix dpager, messed around with page loading some more

--- dpager.c
+++ dpager.c
@@ -6,16 +6,16 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <assert.h>
 #include <signal.h>
 #include <ucontext.h>
-#include <assert.h>
 
 #define STACK_SIZE 20 * sysconf(_SC_PAGE_SIZE)
-#define START_ADDR (void *)0x500000
-extern char **environ;
-Elf64_Addr global_e_entry;
-int elf_fd = -1;
+#define START_ADDR (void *)0xff00000
 
+extern char **environ;
+
+// Struct to hold information about a loadable segment
 typedef struct {
     void *segment_start;
     size_t segment_size;
@@ -27,10 +27,11 @@ ElfSegment loadable_segments[128];
 size_t num_loadable_segments = 0;
 
 void segfault_handler(int sig, siginfo_t *info, void *unused) {
+    // Determine the address that caused the fault
     void *fault_address = info->si_addr;
     size_t page_size = sysconf(_SC_PAGESIZE);
     void *page_start = (void *)((uintptr_t)fault_address & ~(page_size - 1));
-
+    // printf("Page start: %p\n", page_start);
     for (size_t i = 0; i < num_loadable_segments; i++) {
         ElfSegment *seg = &loadable_segments[i];
         if (fault_address >= seg->segment_start && fault_address < (seg->segment_start + seg->segment_size) && seg->file_size > 0) {
@@ -49,7 +50,7 @@ void segfault_handler(int sig, siginfo_t *info, void *unused) {
 
             // Attempt to map the page into memory
             void *mapped = mmap(page_start, page_size, PROT_READ | PROT_WRITE | PROT_EXEC,
-                                MAP_PRIVATE | MAP_FIXED, elf_fd, aligned_file_page_offset);
+                                MAP_PRIVATE | MAP_FIXED, 0, aligned_file_page_offset);
 
             if (mapped == MAP_FAILED) {
                 perror("Demand paging failed");
@@ -105,11 +106,8 @@ void load_elf(const char *filepath, Elf64_Addr *entry_point) {
 
     // Read in the program headers
     Elf64_Phdr phdrs[ehdr.e_phnum];
-    lseek(elf_fd, ehdr.e_phoff, SEEK_SET);
-    if (read(elf_fd, phdrs, sizeof(Elf64_Phdr) * ehdr.e_phnum) != sizeof(Elf64_Phdr) * ehdr.e_phnum) {
-        perror("Failed to read program headers");
-        exit(EXIT_FAILURE);
-    }
+    lseek(fd, ehdr.e_phoff, SEEK_SET);
+    read(fd, phdrs, ehdr.e_phnum * sizeof(Elf64_Phdr));
     
     // Map the segments into memory (if they are loadable)    
     for (int i = 0; i < ehdr.e_phnum; ++i) {
@@ -202,6 +200,7 @@ void stack_check(void* top_of_stack, uint64_t argc, char** argv) {
 }
 
 void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
+    // Get the environment variables
     char **envp = environ;
     int argc, envc, argv_len, envc_len;
 
@@ -222,10 +221,10 @@ void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
         perror("Failed to allocate stack");
         exit(EXIT_FAILURE);
     }
-
+    // Print out mmap call
     printf("mmap call for stack: mmap(addr: %p, size: %ld)\n", START_ADDR, STACK_SIZE);
-    // Might be a good idea here to zero out the memory after??? Not sure
 
+    // Set the stack pointer to the top of the stack
     void *stack_top = START_ADDR + STACK_SIZE;
     // Align stack top to 16-byte boundary
     stack_top = (void *)((uintptr_t)stack_top & -16L);
@@ -233,9 +232,9 @@ void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
     // Calculate the stack pointer locations for envc and argv
     char *stack_past_envc = (char *)stack_top - envc_len;
     char *stack_past_argv = (char *)stack_past_envc - argv_len;
+
     // Copy environment strings and pointers in reverse order to the stack
     int envc_ctr = envc - 1;
-
     while(envp[envc_ctr] != NULL) {
         size_t len = strlen(envp[envc_ctr]) + 1; // Include NULL terminator
         stack_top = ((char *) stack_top) - len;;
@@ -244,15 +243,12 @@ void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
     }
 
     // Copy argv string and pointer to the stack (only one needed, just one program being loaded)
-    // printf("argv[argv_ctr]: %s\n", argv[argc - 1]);
     size_t len = strlen(argv[argc - 1]) + 1; // Include NULL terminator
     stack_top = ((char *) stack_top) - len;
     memcpy(stack_top, argv[argc - 1], len);
-    // strcpy((char *)stack_top, argv[argc - 1]);
 
     // Align stack top to 16-byte boundary
-    stack_top = (unsigned long *)((unsigned long)(stack_top) & ~0xf);
-    // printf("Stack Top after alignment: %p\n", stack_top);
+    stack_top = (void *)((uint64_t)stack_top & -16L);
 
     // Iterate through the env variables to find start of AUX vectors
     for(int i = 0; i < envc & *envp!=NULL; i++){
@@ -260,24 +256,21 @@ void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
     }
     envp++; // Slide past the NULL terminator
     
+    // Find number of AUX vectors
     Elf64_auxv_t *auxv = (Elf64_auxv_t *)envp;
     int aux_num = 0;
-
     while (auxv[aux_num].a_type != AT_NULL) {
         aux_num++;
     }
     aux_num++;
 
     // Allocate space for the auxiliary vector, plus one for the AT_NULL terminator
-    Elf64_auxv_t *aux_vectors_list = calloc(aux_num+1, sizeof(Elf64_auxv_t));
+    Elf64_auxv_t *aux_vectors_list = calloc(aux_num + 1, sizeof(Elf64_auxv_t));
 
     // Copy in the auxiliary vector
     for (int i = 0; i < aux_num; i++) {
         aux_vectors_list[i] = auxv[i];
-        // printf("Aux Vector Element %d", i);
-        // printf(" Type: %ld\n", aux_vector[i].a_type);
     }
-    // printf("length of aux_vector: %d\n", aux_num);
 
     // Adjust the stack pointer to make space for the auxiliary vectors
     size_t total_aux_vector_space = aux_num * sizeof(Elf64_auxv_t);
@@ -289,17 +282,17 @@ void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
 
     // Align stack top to 16-byte boundary
     stack_top = (void *)((uint64_t)stack_top & -16L);
-	*top_of_stack_ptr = (void *)stack_top;
+    *top_of_stack_ptr = (void *)stack_top;
 
-	// Load in the argc, argv, envp, and aux vectors
+    // Load in the argc, argv, envp, and aux vectors
     memcpy(stack_top, &argc, sizeof(argc)); // Copy the value of argc into the stack first
     stack_top += sizeof(argc) * 2; // Align to 8 bits
 
     // Copy the address of argv into the stack
     memcpy(stack_top, &stack_past_argv, sizeof(stack_past_argv));
-    stack_top += sizeof(stack_past_argv) * 2; // Align to 8 bits
+    stack_top += sizeof(stack_past_argv) * 2; // Push stack pointer
 
-    // Copy the addresses of envp into the stack (but not NULL terminator)
+    // Copy the addresses of envp into the stack
     envc_ctr = 0; // Reset counter from earlier
     while (envc_ctr < envc - 1) {
         // Grab each env variable and copy it to the stack
@@ -307,13 +300,14 @@ void setup_stack(void **top_of_stack_ptr, char **argv, Elf64_Addr entry_point) {
         stack_top += sizeof(stack_past_envc);
         envc_ctr++;
     }
-	stack_top += sizeof(stack_past_envc); // Align to 8 bits
+    stack_top += sizeof(stack_past_envc); // Push stack pointer
 
     // Copy the address of the aux vectors into the stack
-	memcpy(stack_top, aux_vectors_list, total_aux_vector_space);
+    memcpy(stack_top, aux_vectors_list, total_aux_vector_space);
+    stack_top += total_aux_vector_space; // Push stack pointer
 
     // Align stack top to 16-byte boundary
-    stack_top += total_aux_vector_space;
+    stack_top = (void *)((uint64_t)stack_top & -16L);
     // Finally, run stack check to verify the stack is set up correctly
     stack_check(*top_of_stack_ptr, argc, argv);
     // Print the stack image (debug output)
@@ -332,7 +326,7 @@ void transfer_control(void *top_of_stack, Elf64_Addr entry_point) {
         "push %%rax\n\t"    // Push the target address onto the stack
         "ret"               // Return, popping the target address off the stack and jumping to it
         :
-        : "r" (top_of_stack), "r" (global_e_entry)
+        : "r" (top_of_stack), "r" (entry_point)
         : "rax", "rbx", "rcx", "rdx", "memory"
     );
 }
@@ -342,10 +336,10 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <ELF file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    setup_signal_handling(); // Setup signal handler for segfaults
     Elf64_Addr entry_point;
     void *top_of_loaded_stack;
 
+    setup_signal_handling(); // Setup signal handler for segfaults
     load_elf(argv[1], &entry_point);
     setup_stack(&top_of_loaded_stack, &argv[1], entry_point);
     transfer_control(top_of_loaded_stack, entry_point);
